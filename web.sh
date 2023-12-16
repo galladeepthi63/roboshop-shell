2629d08e8b59ef7d08f6d4bb035dedc8653be725
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-16 19:38:25 +0530

    create we application

--- web.sh
+++ web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -7,50 +8,60 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-#MONGODB_HOST= 172.31.34.122
+
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-        if [ $1 -ne 0 ]
-        then 
-            echo -e "$2 ...$R FAILED $N"
-        else
-            echo -e "$2...$G SUCESS $N"
-        fi
-    }
-    if [ $ID -ne 0 ] 
-    then 
-        echo "ERROR:: Please run this script with root user "
-        exit 1 # you can give other than 0
-    else 
-        echo "you are root user "
-    fi # fi means reverse of if, indicating condition end
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
-    dnf install nginx -y &>> $LOGFILE
-    VALIDATE $? "Install Nginx"
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-    systemctl enable nginx &>> $LOGFILE
-    VALIDATE $? "Enable Nginx service"
+dnf install nginx -y &>> $LOGFILE
+ 
+VALIDATE $? "Installing nginx"
 
-    systemctl start nginx &>> $LOGFILE
-    VALIDATE $? "Start Nginx service"
+systemctl enable nginx &>> $LOGFILE
 
-    rm -rf /usr/share/nginx/html/* &>> $LOGFILE
-    VALIDATE $? "remove the web server "
+VALIDATE $? "Enable nginx" 
 
-    curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
-    VALIDATE $? "Downloading the web application "
+systemctl start nginx &>> $LOGFILE
 
-    cd /usr/share/nginx/html &>> $LOGFILE
-    VALIDATE $? "moving nginx html directory"
+VALIDATE $? "Starting Nginx"
 
-    unzip /tmp/web.zip &>> $LOGFILE
-    VALIDATE $? "Unzip web application "
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 
-    cp /home/centos/roboshop-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
-    VALIDATE $? "Copying the roboshop "
+VALIDATE $? "removed default website"
 
-    systemctl restart nginx &>> $LOGFILE
-    VALIDATE $? "restarted nginx"
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
 
+VALIDATE $? "Downloaded web application"
+
+cd /usr/share/nginx/html &>> $LOGFILE
+
+VALIDATE $? "moving nginx html directory"
+
+unzip -o /tmp/web.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping web"
+ 
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE 
+
+VALIDATE $? "copied roboshop reverse proxy config"
+
+systemctl restart nginx &>> $LOGFILE
+
+VALIDATE $? "restarted nginx"
