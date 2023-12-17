610f46b0076769591e0e7d3f31f414c3d0f0fdd3
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-17 15:38:45 +0530

    update cart

--- cart.sh
+++ cart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -10,60 +13,77 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-        if [ $1 -ne 0 ]
-        then 
-            echo -e "$2 ...$R FAILED $N"
-        else
-            echo -e "$2...$G SUCESS $N"
-        fi
-    }
-    if [ $ID -ne 0 ] 
-    then 
-        echo "ERROR:: Please run this script with root user "
-        exit 1 # you can give other than 0
-    else 
-        echo "you are root user "
-    fi # fi means reverse of if, indicating condition end
-dnf module disable nodejs -y &>> $LOGFILE
-VALIDATE $? "disable the nodejs"
-
-dnf module enable nodejs:18 -y &>> $LOGFILE
-VALIDATE $? "enable the nodejs"
-
-dnf install nodejs -y &>> $LOGFILE
-VALIDATE $? "install the nodejs"
-
- id roboshop
-    if [ $? -ne 0 ]
-    then 
-        useradd  roboshop
-        VALIDATE $? "added the roboshop" 
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
     else
-        echo -e "roboshop user already exist  $Y SKIPPING $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
+}
 
-    mkdir -p /app
-    VALIDATE $? "creating the app directory"
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>> $LOGFILE
-VALIDATE $? "Download the cart application"
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current NodeJS"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
+VALIDATE $? "Enabling NodeJS:18"
+
+dnf install nodejs -y  &>> $LOGFILE
+
+VALIDATE $? "Installing NodeJS:18"
+
+id roboshop #if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip  &>> $LOGFILE
+
+VALIDATE $? "Downloading cart application"
 
 cd /app 
-unzip /tmp/cart.zip &>> $LOGFILE
-VALIDATE $? "unzip the cart application"
 
-cd /app 
+unzip -o /tmp/cart.zip  &>> $LOGFILE
+
+VALIDATE $? "unzipping cart"
+
 npm install  &>> $LOGFILE
-VALIDATE $? "Download the depences"
 
+VALIDATE $? "Installing dependencies"
+
+# use absolute, because cart.service exists there
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>> $LOGFILE
-VALIDATE $? "copying the car service"
+
+VALIDATE $? "Copying cart service file"
 
 systemctl daemon-reload &>> $LOGFILE
-VALIDATE $? "reload the daemon"
 
-systemctl enable cart  &>> $LOGFILE
+VALIDATE $? "cart daemon reload"
+
+systemctl enable cart &>> $LOGFILE
 VALIDATE $? "Enable cart"
 
 systemctl start cart &>> $LOGFILE
-VALIDATE $? "Start  the cart servie"
+
+VALIDATE $? "Starting cart"
+
+
+
