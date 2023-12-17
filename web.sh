cdfcb7bda6293ad8fdb87b1381f4648b6f7171ec
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-17 13:47:32 +0530

    update web server

--- web.sh
+++ web.sh
@@ -5,10 +5,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+MONGDB_HOST=mongodb.daws76s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
 
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
