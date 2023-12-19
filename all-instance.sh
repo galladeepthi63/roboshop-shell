fa294972e74268ad72059a9cc12ff4ae19ab7c09
Ramadeepthi Galla (HCL TECHNOLOGIES CORPORATE SER) <v-ragalla@microsoft.com>
2023-12-19 19:04:40 +0530

    create the all instance

--- all-instance.sh
+++ all-instance.sh
@@ -3,6 +3,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-0161bbc0ff4c0e791
 INSTANCES=("mongodb" "redis" "mysql" "cart" "rabbitmq" "catalogue")
+ZONE_ID=Z089758214EN5STGRKQNW
+DOMAIN_NAME="ramakrishna.cloud"
 
 for i in "${INSTANCES[@]}"
     do 
@@ -17,7 +19,28 @@ for i in "${INSTANCES[@]}"
 
         IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb  --instance-type $INSTANCE_TYPE  --security-group-ids sg-0161bbc0ff4c0e791 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
         echo "$i: $IP_ADDRESS"
-    done
+
+        #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch "
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    } "
+
+done
+
 
 
 
